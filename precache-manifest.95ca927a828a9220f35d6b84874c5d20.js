c3827db5139a6ffd5fd0051b3f611876ed830d71
Nick Lee <lenchol@amazon.com>
2020-09-14 18:48:49 +0100

    Updates

---
+++ precache-manifest.95ca927a828a9220f35d6b84874c5d20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc89890b3f74b7c98343e9c52275a5e7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5b891ec27508a3f17f20",
+    "url": "./static/css/2.d4e7eff5.chunk.css"
+  },
+  {
+    "revision": "dcde25496fad2d810ba2",
+    "url": "./static/css/main.35f92c17.chunk.css"
+  },
+  {
+    "revision": "5b891ec27508a3f17f20",
+    "url": "./static/js/2.96aa4bc1.chunk.js"
+  },
+  {
+    "revision": "5de3d6d724638e8143c8b227673229ae",
+    "url": "./static/js/2.96aa4bc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcde25496fad2d810ba2",
+    "url": "./static/js/main.067e6ff8.chunk.js"
+  },
+  {
+    "revision": "29ea3fb9c18ae445d5b9",
+    "url": "./static/js/runtime-main.89ead38b.js"
+  }
+]);
