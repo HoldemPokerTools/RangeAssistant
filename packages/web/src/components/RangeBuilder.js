a3fd3f404540b48d0e147d5e93a154ce4440a545
Nick Lee <lenchol@amazon.com>
2020-09-29 09:03:17 +0100

    add more colors

--- packages/web/src/components/RangeBuilder.js
+++ packages/web/src/components/RangeBuilder.js
@@ -18,11 +18,18 @@ const { Option } = Select;
 
 const colors = [
   { value: "#d3d3d3", name: "Grey" },
-  { value: "#efe819", name: "Yellow" },
-  { value: "#06ce0c", name: "Green" },
-  { value: "#639dda", name: "Blue" },
+  { value: "#e89679", name: "Peach" },
+  { value: "#d9e90e", name: "Yellow" },
+  { value: "#7ec78e", name: "Green" },
+  { value: "#6d9ec2", name: "Blue" },
   { value: "#bb63fd", name: "Purple" },
   { value: "#fd6363", name: "Red" },
+  { value: "#ea9900", name: "Orange" },
+  { value: "#46cec0", name: "Turquoise" },
+  { value: "#ffffff", name: "White" },
+  { value: "#e09cc5", name: "Pink" },
+  { value: "#8a0000", name: "Maroon" },
+  { value: "#06ce0c", name: "Bright Green" },
 ];
 
 function RangeBuilder({ onChange }) {
