c273bccfd5e91540c57d9ae00c58ce03b792a80c
HieronymusLex <HieronymusLex@users.noreply.github.com>
2020-10-12 08:23:13 +0000

    deploy: ece9cd80f9c99fc67791bd230e3becd9d936492d

---
+++ precache-manifest.74d3f14d1dbfcc84df337fc78dbed347.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c0381e2906c0fe831809aae2c06f256",
+    "url": "./index.html"
+  },
+  {
+    "revision": "efa5b2efe70873f7a9b5",
+    "url": "./static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "4e23a71b02a313103f06",
+    "url": "./static/css/main.040a8ee8.chunk.css"
+  },
+  {
+    "revision": "efa5b2efe70873f7a9b5",
+    "url": "./static/js/2.db452175.chunk.js"
+  },
+  {
+    "revision": "5de3d6d724638e8143c8b227673229ae",
+    "url": "./static/js/2.db452175.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e23a71b02a313103f06",
+    "url": "./static/js/main.ad88938d.chunk.js"
+  },
+  {
+    "revision": "88c5f6ed4b451ce04d27",
+    "url": "./static/js/runtime-main.d878fd67.js"
+  }
+]);
