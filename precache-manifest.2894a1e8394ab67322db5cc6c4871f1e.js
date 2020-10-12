2441082cdbc671b14bd02c29fed50c6ac8157bfa
HieronymusLex <HieronymusLex@users.noreply.github.com>
2020-10-12 08:23:05 +0000

    deploy: fedba175bab027a6913c3a80178c2a0d6370d087

---
+++ precache-manifest.2894a1e8394ab67322db5cc6c4871f1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "327d58613cae041d9e86b23931e5d928",
+    "url": "./index.html"
+  },
+  {
+    "revision": "00e582978527bba3d802",
+    "url": "./static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "75dfe50eb4cb0c11ab12",
+    "url": "./static/css/main.040a8ee8.chunk.css"
+  },
+  {
+    "revision": "00e582978527bba3d802",
+    "url": "./static/js/2.eedb8529.chunk.js"
+  },
+  {
+    "revision": "5de3d6d724638e8143c8b227673229ae",
+    "url": "./static/js/2.eedb8529.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75dfe50eb4cb0c11ab12",
+    "url": "./static/js/main.c350a044.chunk.js"
+  },
+  {
+    "revision": "88c5f6ed4b451ce04d27",
+    "url": "./static/js/runtime-main.d878fd67.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.74d3f14d1dbfcc84df337fc78dbed347.js"
+  "./precache-manifest.2894a1e8394ab67322db5cc6c4871f1e.js"
 );
 
 self.addEventListener('message', (event) => {
